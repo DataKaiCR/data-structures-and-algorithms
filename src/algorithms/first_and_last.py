5421c3daab006c1f7f4c82a3cc1c352a29a3c078
datakai57 <hstecher@datakai.net>
2023-02-01 21:58:33 -0600

    Add new method and finish testing

--- src/algorithms/first_and_last.py
+++ src/algorithms/first_and_last.py
@@ -12,7 +12,7 @@
             - the last 1 in the array is in position 2: arr[2] = 1
 '''
 
-from typing import List
+from typing import List, Optional
 
 
 class FirstAndLast:
@@ -23,14 +23,54 @@ class FirstAndLast:
         self.arr = sorted(arr)
         self.target = target
 
+
+    def base_case(self) -> Optional[List[int]]:
+        return [-1,-1] if len(self.arr) == 0 or self.arr[0] > self.target or self.arr[-1] < self.target else None
+
+
     def compute_by_iteration(self) -> List[int]:
-        # Go until each item in the array until we find a match. If no match we default to [-1,-1]
-        for i in range(len(self.arr)):
-            # Match found
-            if self.arr[i] == self.target:
-                start = i
-                # Keep incrementing the array index + 1 until the target is different from value
-                while i + 1 < len(self.arr) and self.arr[i + 1] == self.target:
-                    i += 1
-                return [start, i]
-        return [-1, -1]
+        if self.base_case() is None:
+            # Go until each item in the array until we find a match. If no match we default to [-1,-1]
+            for i in range(len(self.arr)):
+                # Match found
+                if self.arr[i] == self.target:
+                    start = i
+                    # Keep incrementing the array index + 1 until the target is different from value
+                    while i + 1 < len(self.arr) and self.arr[i + 1] == self.target:
+                        i += 1
+                    return [start, i]
+        return [-1,-1]
+
+
+    def _find_start(self) -> int:
+        if self.arr[0] == self.target:
+            return 0
+        left, right = 0, len(self.arr) - 1
+        while left <= right:
+            mid = (left + right)//2
+            if self.arr[mid] == self.target and self.arr[mid- 1]  < self.target:
+                return mid
+            elif self.arr[mid] < self.target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return - 1
+
+    def _find_end(self) -> int:
+        if self.arr[-1] == self.target:
+            return len(self.arr) -1
+        left, right = 0, len(self.arr) - 1
+        while left <= right:
+            mid = (left + right)//2
+            if self.arr[mid] == self.target and self.arr[mid+ 1] > self.target:
+                return mid
+            elif self.arr[mid] > self.target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return - 1
+
+    def compute_by_binary_search(self) -> List[int]:
+        if self.base_case() is None:
+            return [self._find_start(), self._find_end()]
+        return [-1,-1]
--- tests/test_first_and_last.py
+++ tests/test_first_and_last.py
@@ -11,7 +11,83 @@ def test_class_instance() -> None:
     ('x', 0, 'You must supply an array'),
     ([1], 'y', 'Your target must be a positive integer')
 ])
-def class_instance_fail_by_nonvalid_parameters(input1, input2, expected) -> None:
+def test_class_instance_fail_by_nonvalid_parameters(input1, input2, expected) -> None:
     with pytest.raises(AssertionError) as context:
-        FirstAndLast(word1=input1, word2=input2)
+        FirstAndLast(arr=input1, target=input2)
     assert (expected in str(context.value))
+
+
+@pytest.mark.parametrize("input1, input2, expected", [
+    ([], 0, [-1, -1]),
+    ([1], 0, [-1, -1]),
+    ([0], 1, [-1, -1]),
+    ([1, 1, 1, 1, 1, 1, 1], 9, [-1, -1])
+])
+def test_base_case(input1, input2, expected):
+    obj = FirstAndLast(input1, input2)
+    assert obj.base_case() == expected
+
+
+def test_compute_by_iteration_sorted():
+    obj = FirstAndLast([1, 2, 4, 5, 5, 7, 8], 5)
+    assert obj.compute_by_iteration() == [3, 4]
+
+
+def test_compute_by_iteration_non_sorted():
+    obj = FirstAndLast([5, 7, 2, 4, 8, 5, 1], 5)
+    assert obj.compute_by_iteration() == [3, 4]
+
+
+def test_compute_by_iteration_not_found():
+    obj = FirstAndLast([5, 1, 1, 1, 1, 1, 1], 9)
+    assert obj.compute_by_iteration() == [-1, -1]
+
+
+def test_find_start():
+    obj = FirstAndLast([1, 2, 4, 5, 5, 7, 8], 5)
+    assert obj._find_start() == 3
+
+
+def test_find_start_first_item():
+    obj = FirstAndLast([1, 2, 4, 5, 5, 7, 8], 1)
+    assert obj._find_start() == 0
+
+
+def test_find_start_target_greaterthan_mid():
+    obj = FirstAndLast([1, 2, 3, 4, 5, 6], 4)
+    assert obj._find_start() == 3
+
+
+def test_find_start_fail():
+    obj = FirstAndLast([1, 2, 3, 4, 5, 6], 0)
+    assert obj._find_start() == -1
+
+
+def test_find_end():
+    obj = FirstAndLast([1, 2, 4, 5, 5, 7, 8], 5)
+    assert obj._find_end() == 4
+
+
+def test_find_end_last_item():
+    obj = FirstAndLast([1, 2, 4, 5, 5, 7, 8], 8)
+    assert obj._find_end() == 6
+
+
+def test_find_end_target_lessthan_mid():
+    obj = FirstAndLast([1, 2, 3, 4, 5, 6], 2)
+    assert obj._find_end() == 1
+
+
+def test_find_end_fail():
+    obj = FirstAndLast([1, 2, 3, 4, 5, 6], 7)
+    assert obj._find_end() == - 1
+
+
+def test_compute_by_binary_search():
+    obj = FirstAndLast([1, 2, 4, 5, 5, 7, 8], 5)
+    assert obj.compute_by_binary_search() == [3, 4]
+
+
+def test_compute_by_binary_search_fail():
+    obj = FirstAndLast([1, 2, 4, 5, 5, 7, 8], 9)
+    assert obj.compute_by_binary_search() == [-1, -1]
